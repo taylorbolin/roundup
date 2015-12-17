5ad2febe9bce81c71b383a36146925be089ed7f0
Taylor Bolin <t.n.bolin@gmail.com>
2015-12-17 11:54:16 -0800

    changed services.js

--- public/app/services.js
+++ public/app/services.js
@@ -1,4 +1,4 @@
 angular.module('RoundUpServices', ['ngResource'])
 .factory('RoundUp', ['$resource', '$routeParams', function($resource, $routeParams) {
-	return $resource('http://localhost:3000/api/roundups/:id');
+	return $resource('/api/roundups/:id');
 }])
